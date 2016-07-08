8b89e74cc571ad1c0dd0d7aa1e57d398650ce84c
Vinicius Hagamenon <vinicius@movedigital.com.br>
2016-07-08 02:48:15 -0300

    modified footer to a stateless component

--- client/js/components/Footer.js
+++ client/js/components/Footer.js
@@ -8,45 +8,43 @@
  */
 'use strict'
 
-import React, { Component } from 'react'
+import React from 'react'
 
 import { SvgIcon } from '../utils'
 
-class Footer extends Component {
-  render() {
-    return (
-      <footer className="main-footer">
-        <div className="container">
-          <div className="widget-address">
-            <span className="state">RJ</span>
-            <address className="address">
-              Rua do acre, 77 - sala 1108<br />
-              Centro - Rio de Janeiro<br />
-              <a href="tel:+552122636044" className="phone">tel.: +55 (21) 2263.6044</a>
-            </address>
-          </div>
-          <div className="widget-address">
-            <span className="state">SP</span>
-            <address className="address">
-              Rua Cardoso de melo, 1750<br />
-              6º Andar Vila Olímpia<br />
-              <a href="tel:+552122636044" className="phone">tel.: +55 (21) 2263.6044</a>
-            </address>
-          </div>
-          <div className="widget-contact">
-            <a className="contact-button" href="mailto:vinicius_neo@hotmail.com">
-              <SvgIcon icon="email" />
-              <span>ENTRE EM <strong>CONTATO</strong></span>
-            </a>
-            <a className="contact-button" href="#consultor-online">
-              <SvgIcon icon="talk-with-us" />
-              <span>FALE COM O NOSSO <strong>CONSULTOR ONLINE</strong></span>
-            </a>
-          </div>
+const Footer = () => {
+  return (
+    <footer className="main-footer">
+      <div className="container">
+        <div className="widget-address">
+          <span className="state">RJ</span>
+          <address className="address">
+            Rua do acre, 77 - sala 1108<br />
+            Centro - Rio de Janeiro<br />
+            <a href="tel:+552122636044" className="phone">tel.: +55 (21) 2263.6044</a>
+          </address>
         </div>
-      </footer>
-    )
-  }
+        <div className="widget-address">
+          <span className="state">SP</span>
+          <address className="address">
+            Rua Cardoso de melo, 1750<br />
+            6º Andar Vila Olímpia<br />
+            <a href="tel:+552122636044" className="phone">tel.: +55 (21) 2263.6044</a>
+          </address>
+        </div>
+        <div className="widget-contact">
+          <a className="contact-button" href="mailto:vinicius_neo@hotmail.com">
+            <SvgIcon icon="email" />
+            <span>ENTRE EM <strong>CONTATO</strong></span>
+          </a>
+          <a className="contact-button" href="#consultor-online">
+            <SvgIcon icon="talk-with-us" />
+            <span>FALE COM O NOSSO <strong>CONSULTOR ONLINE</strong></span>
+          </a>
+        </div>
+      </div>
+    </footer>
+  )
 }
 
 export default Footer
