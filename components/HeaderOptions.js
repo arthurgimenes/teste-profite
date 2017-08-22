d5d52f4500a01369e9e2c779c43b96e15849dd8a
nathaliabruno <nathi.bruno@gmail.com>
2017-08-22 15:42:35 -0300

    TGT | Styling header

--- components/HeaderOptions.js
+++ components/HeaderOptions.js
@@ -1,17 +1,17 @@
 export const HeaderOptions = () => (
   <div className="header-options">
     <ul className="header-options-languages">
-      <li>
+      <li className="header-options-languages-flag">
         <a href="javascript:void(0)">
           <img src="/static/image/header/bandeira1.png" />
         </a>
       </li>
-      <li>
+      <li className="header-options-languages-flag">
         <a href="javascript:void(0)">
           <img src="/static/image/header/bandeira2.png" />
         </a>
       </li>
-      <li>
+      <li className="header-options-languages-flag">
         <a href="javascript:void(0)">
           <img src="/static/image/header/bandeira3.png" />
         </a>
@@ -20,7 +20,9 @@ export const HeaderOptions = () => (
     <div className="header-options-contact">
       <a href="tel:+551133375618">
         <img src="/static/image/header/skype.png" />
-        11 3337 5618
+        <span className="header-options-contact-number">
+          11 3337 5618
+        </span>
       </a>
     </div>
   </div>
--- components/Search.js
+++ components/Search.js
@@ -1,8 +1,11 @@
 export const Search = () => (
   <form className="search">
-      <button>
+      <button className="search-button">
         <img src="/static/image/header/search.png" />
       </button>
-      <input type="text" placeholder="Buscar por ..." />
+      <input 
+        type="text" 
+        placeholder="Buscar por ..." 
+        className="search-input" />
   </form>
 )
