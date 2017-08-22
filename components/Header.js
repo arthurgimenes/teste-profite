1aa50eb1dba92214ba8bcf15e6c565b32c3fc6fb
nathaliabruno <nathi.bruno@gmail.com>
2017-08-22 00:50:49 -0300

    TGT | Setup Container

--- components/Header.js
+++ components/Header.js
@@ -8,14 +8,18 @@ export default class Header extends React.Component {
   render () {
     return (
       <header className="header">
-        <div className="header-brand">
-          <h1>
-            <img src="/static/image/header/profite_logo_novo.png" />
-          </h1>
+        <div className="header-container">
+          <div className="header-brand">
+            <h1>
+              <img 
+                src="/static/image/header/profite_logo_novo.png"
+                className="header-brand-image" />
+            </h1>
+          </div>
+          <Welcome name="Visitante"/>
+          <Search />
+          <HeaderOptions />
         </div>
-        <Welcome name="Visitante"/>
-        <Search />
-        <HeaderOptions />
       </header>
     )
   }
