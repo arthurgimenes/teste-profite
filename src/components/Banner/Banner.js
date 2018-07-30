66bd74fe07b23aa26be07e6c9ffcea249d95513a
Glauber Mota <glaubernm@gmail.com>
2018-07-30 08:04:03 -0300

    Cabeçalho: Fake API, resize... 100%

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -2,6 +2,43 @@ import React, { Component } from 'react';
 import Slider from "react-slick";
 
 class Banner extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      slides: [],
+      bannerHeight: "auto"
+    };
+  }
+
+  updateBannerHeight() {
+    const bannerHeight = document.getElementById("banner").clientHeight;
+    const containers = document.getElementsByClassName("banner__container");
+
+    for (let index = 0; index < containers.length; index++) {
+      containers[index].setAttribute("style", "height: " + bannerHeight + "px");
+    }
+  }
+
+  componentDidMount() {
+    fetch("/api/banner.json")
+      .then(res => res.json())
+      .then(
+        (result) => { this.setState({ slides: result }); },
+        (error) => { this.setState({ error }); }
+      );
+
+    window.addEventListener('resize', this.updateBannerHeight);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateBannerHeight);
+  }
+
+  componentDidUpdate() {
+    this.updateBannerHeight();
+  }
+
   splitWords(phrase) {
     const splitString = phrase.split(' ');
 
@@ -13,10 +50,13 @@ class Banner extends Component {
   }
 
   render() {
+    const slides = this.state.slides;
+    const error = this.state.error;
+    // const bannerHeight = this.state.bannerHeight;
     const settings = {
       useCSS: false,
       useTransform: false,
-      autoplay: true,
+      // autoplay: true,
       autoplaySpeed: 3000,
       pauseOnDotsHover: true,
       pauseOnHover: false,
@@ -37,50 +77,33 @@ class Banner extends Component {
       ]
     };
 
+    if (error) console.log(error.message);
+
     return (
-      <div className="banner">
+      <div className="banner" id="banner">
 
         <Slider {...settings}>
 
-          <div>
-            <div className="banner__slide">
+          {slides.map(slide => (
+            <div key={slide.id} className="banner__slide">
               <div className="banner__bg">
                 <div className="banner__spacer"></div>
                 <div className="banner__img"
                   style={{
-                    backgroundImage: "url(" + require("../../assets/img/banner/carousel-1.png") + ")"
+                    backgroundImage: "url(" +
+                      require("../../assets/img/banner/" + slide.image)
+                      + ")"
                   }}
                 ></div>
               </div>
               <div className="container banner__container">
                 <h3 className="banner__heading">
-                  {this.splitWords('Nossa especialidade: experiência de compra.')}
+                  {this.splitWords(slide.title)}
                 </h3>
               </div>
             </div>
-          </div>
+          ))}
 
-          <div>
-            <div className="container banner__container">
-              <h3 className="banner__heading">
-                2
-              </h3>
-            </div>
-          </div>
-          <div>
-            <div className="container banner__container">
-              <h3 className="banner__heading">
-                3
-              </h3>
-            </div>
-          </div>
-          <div>
-            <div className="container banner__container">
-              <h3 className="banner__heading">
-                4
-              </h3>
-            </div>
-          </div>
         </Slider>
 
       </div>
