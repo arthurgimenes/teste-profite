ecf1b65e434e48924b64fd7c6d9bbc00ebe19fc4
MatthausZillig <matthaus.zillig@proatecnologia.com.br>
2019-03-06 13:41:10 -0300

    Criação do componente headerNav WIP

---
+++ teste-profite-matthausDev/src/components/HeaderNav.vue
@@ -0,0 +1,18 @@
+<template>
+  <header class="row">
+    <div class="col-sm-12 col-md-12 col-lg-12 header-height">
+        <img src="../assets/profite_logo.png" alt="logo profite" width="60" height="60">
+        <div class="input-wrapper">
+          <input class="input-align" type="text" name="search" placeholder="O que está procurando?">
+        </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+name: "HeaderNav"
+}
+</script>
+
+
--- teste-profite-matthausDev/src/main.js
+++ teste-profite-matthausDev/src/main.js
@@ -5,6 +5,7 @@ import App from './App'
 import router from './router'
 import './styles/variables.scss'
 import './styles/my-flexbox.scss'
+import './styles/style.scss'
 
 Vue.config.productionTip = false
 
