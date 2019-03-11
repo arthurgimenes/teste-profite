591499cbe9eced0f50b3ae88fb3ec5a278aab798
Rogerio Orioli <rogerio@designhope.com.br>
2019-03-11 17:35:55 -0300

    start footer

--- profit/src/Components/Footer/Footer.js
+++ profit/src/Components/Footer/Footer.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import Title from '../Title/Title'
 import './footer.scss'
+import iconContact from './contact.svg'
+import iconMail from './mail.svg'
+import Button from '../Button/Button'
 const Footer = (props) => {
 
   return(
@@ -9,6 +12,39 @@ const Footer = (props) => {
         <div className="row">
           <div className="col-md-12">
             <Title title="Localização" color='#fff' />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-4">
+            <div className="row">
+            <div className="col-md-6">
+            <h4>São Paulo</h4>  
+            Rua do Rócio, 423/1801
+            Vila Olímpia - SP
+            04552-000
+            +55 11  3333 3333
+            </div>
+            <div className="col-md-6">
+            <h4>Rio de Janeiro</h4>
+            Vol. da Pátria, 301/702
+            Botafogo - RJ
+            22270-000
+            +55 21  3333 3333
+            </div>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="row">
+              <div className="col-md-6 col-xs-6">
+                <Button src={iconMail} title="entre em contato"/>
+                <Button src={iconContact} title="FALE COM O NOSSO CONSULTOR ONLINE"/>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="row">
+
+            </div>
           </div>
         </div>
       </div>
